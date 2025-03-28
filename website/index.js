6a7d047c9f9104cc7365487c38ba039a84f307b5
sonny sayaloune <ashunterminator@gmail.com>
2025-03-29 02:13:29 +1100

    added first files

---
+++ website/index.js
@@ -0,0 +1,3 @@
+document.getElementById("intro").textContent = "Sonny's First Projec.t";
+document.getElementById("desc").textContent = "A website to help get me started on my coding journey.";
+document.getElementById("about").textContent = "Website will primarily be about hobbies and interests.";
