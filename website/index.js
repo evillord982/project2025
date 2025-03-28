9866b48de8e42fbb06c333132ab8b4a79a7e268a
sonny sayaloune <ashunterminator@gmail.com>
2025-03-29 02:38:30 +1100

    test

--- website/index.js
+++ website/index.js
@@ -1,3 +1,4 @@
+//testing if this will commit to git repo
 document.getElementById("intro").textContent = "Sonny's First Projec.t";
 document.getElementById("desc").textContent = "A website to help get me started on my coding journey.";
 document.getElementById("about").textContent = "Website will primarily be about hobbies and interests.";
